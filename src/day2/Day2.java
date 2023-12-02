1521eb119d1f605ce2c859ff45b09ad3f8ab12e7
Omar Chatila <omar.chatila@tu-dortmund.de>
2023-12-02 20:32:52 +0100

    refactor day2 code

--- src/day2/Day2.java
+++ src/day2/Day2.java
@@ -21,35 +21,22 @@ public class Day2 {
     }
 
     public static int getResult() {
+        return processData((maxGreen, maxBlue, maxRed, gameID) -> maxGreen <= GREEN_THRESHOLD
+                && maxBlue <= BLUE_THRESHOLD && maxRed <= RED_THRESHOLD ? gameID : 0);
+    }
+
+    public static int getResult2() {
+        return processData((maxGreen, maxBlue, maxRed, gameID) -> maxGreen * maxBlue * maxRed);
+    }
+
+    private static int processData(TriColorOperation operation) {
         int result = 0;
         try {
             List<String> content = Files.readAllLines(Paths.get("src/day2/input.txt"));
             int gameID = 1;
             for (String entry : content) {
-                entry = entry.replaceAll("Game \\d*: ", "");
-                StringTokenizer tokenizer = new StringTokenizer(entry, ";");
-                int maxGreen = 0, maxRed = 0, maxBlue = 0;
-                while (tokenizer.hasMoreElements()) {
-                    String set = tokenizer.nextToken();
-                    StringTokenizer tokenizer2 = new StringTokenizer(set, ",");
-                    while (tokenizer2.hasMoreElements()) {
-                        String pair = tokenizer2.nextToken();
-                        if (pair.contains("green")) {
-                            int green = Integer.parseInt(pair.replaceAll("\\D*", ""));
-                            maxGreen = Math.max(green, maxGreen);
-                        } else if (pair.contains("blue")) {
-                            int blue = Integer.parseInt(pair.replaceAll("\\D*", ""));
-                            maxBlue = Math.max(blue, maxBlue);
-                        } else {
-                            int red = Integer.parseInt(pair.replaceAll("\\D*", ""));
-                            maxRed = Math.max(red, maxRed);
-                        }
-                    }
-                }
-                if (maxGreen <= GREEN_THRESHOLD && maxBlue <= BLUE_THRESHOLD && maxRed <= RED_THRESHOLD) {
-                    result += gameID;
-                }
-                gameID++;
+                Tuple<Integer, Integer, Integer> colorTuples = getTuples(entry);
+                result += operation.execute(colorTuples.t1(), colorTuples.t2(), colorTuples.t3(), gameID++);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -57,37 +44,35 @@ public class Day2 {
         return result;
     }
 
-    public static int getResult2() {
-        int result = 0;
-        try {
-            List<String> content = Files.readAllLines(Paths.get("src/day2/input.txt"));
-            for (String entry : content) {
-                entry = entry.replaceAll("Game \\d*: ", "");
-                StringTokenizer tokenizer = new StringTokenizer(entry, ";");
-                int minGreen = 0, minRed = 0, minBlue = 0;
-                while (tokenizer.hasMoreElements()) {
-                    String set = tokenizer.nextToken();
-                    StringTokenizer tokenizer2 = new StringTokenizer(set, ",");
-                    while (tokenizer2.hasMoreElements()) {
-                        String pair = tokenizer2.nextToken();
-                        if (pair.contains("green")) {
-                            int green = Integer.parseInt(pair.replaceAll("\\D*", ""));
-                            minGreen = Math.max(green, minGreen);
-                        } else if (pair.contains("blue")) {
-                            int blue = Integer.parseInt(pair.replaceAll("\\D*", ""));
-                            minBlue = Math.max(blue, minBlue);
-                        } else {
-                            int red = Integer.parseInt(pair.replaceAll("\\D*", ""));
-                            minRed = Math.max(red, minRed);
-                        }
-                    }
+    private static Tuple<Integer, Integer, Integer> getTuples(String entry) {
+        entry = entry.replaceAll("Game \\d*: ", "");
+        StringTokenizer tokenizer = new StringTokenizer(entry, ";");
+        int maxGreen = 0, maxBlue = 0, maxRed = 0;
+        while (tokenizer.hasMoreElements()) {
+            String set = tokenizer.nextToken();
+            StringTokenizer tokenizer2 = new StringTokenizer(set, ",");
+            while (tokenizer2.hasMoreElements()) {
+                String pair = tokenizer2.nextToken();
+                if (pair.contains("green")) {
+                    int green = Integer.parseInt(pair.replaceAll("\\D*", ""));
+                    maxGreen = Math.max(green, maxGreen);
+                } else if (pair.contains("blue")) {
+                    int blue = Integer.parseInt(pair.replaceAll("\\D*", ""));
+                    maxBlue = Math.max(blue, maxBlue);
+                } else {
+                    int red = Integer.parseInt(pair.replaceAll("\\D*", ""));
+                    maxRed = Math.max(red, maxRed);
                 }
-                int power = minBlue * minRed * minGreen;
-                result += power;
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
-        return result;
+        return new Tuple<>(maxGreen, maxBlue, maxRed);
+    }
+
+    private interface TriColorOperation {
+        int execute(int green, int blue, int red, int gameID);
+    }
+
+    private record Tuple<T1, T2, T3>(T1 t1, T2 t2, T3 t3) {
     }
 }
+
