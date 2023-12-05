7c6a4a7b3e94fa390868c8e2a2801bc1260ed5a5
omarc <sgtb45@gmail.com>
2023-12-05 16:46:25 +0100

    Day 5, part 2

--- src/day5/Day5.java
+++ src/day5/Day5.java
@@ -4,11 +4,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Day5 {
+
     record Map(long destRangeStart, long sourceRangeStart, long range) {
         @Override
         public String toString() {
@@ -16,16 +15,17 @@ public class Day5 {
         }
     }
 
-    record Corresponding(long source, long dest) {
+    record Interval(long start, long end) {
         @Override
         public String toString() {
-            return source + "-" + dest;
+            return "[" + start + ", " + end + "]";
         }
     }
 
     static List<List<Map>> sourceToDestList = new ArrayList<>();
-    static Long[] nextSeed;// = new ArrayList<>();
+    static Long[] nextSeed;
     static List<Long> seeds = new ArrayList<>();
+    static final int maxRangeSize = (int) 1E6;
 
     static void setData() throws IOException {
         for (int i = 0; i < 7; i++) {
@@ -35,8 +35,9 @@ public class Day5 {
         int index = -1;
         for (String line : lines) {
             if (line.startsWith("seeds")) {
-                for (String s : line.substring(line.indexOf(": ") + 2).split("\\s+"))
+                for (String s : line.substring(line.indexOf(": ") + 2).split("\\s+")) {
                     seeds.add(Long.parseLong(s));
+                }
             } else {
                 if (line.isEmpty()) {
                     index++;
@@ -50,36 +51,84 @@ public class Day5 {
         }
     }
 
-    private static long getResult() {
+    static List<Interval> setIntervals(List<Long> numbers) {
+        List<Interval> intervals = new ArrayList<>();
+        for (int i = 0; i < numbers.size(); i += 2) {
+            long start = numbers.get(i);
+            long end = start + numbers.get(i + 1);
+            if (end - start > Day5.maxRangeSize) {
+                for (long subStart = start; subStart < end; subStart += Day5.maxRangeSize) {
+                    long subEnd = Math.min(subStart + Day5.maxRangeSize, end);
+                    intervals.add(new Interval(subStart, subEnd));
+                }
+            } else {
+                List<Interval> overlappingIntervals = new ArrayList<>();
+                for (Interval interval : intervals) {
+                    if (!(end < interval.start() || start > interval.end())) {
+                        overlappingIntervals.add(interval);
+                    }
+                }
+                for (Interval overlap : overlappingIntervals) {
+                    intervals.remove(overlap);
+                    start = Math.min(start, overlap.start());
+                    end = Math.max(end, overlap.end());
+                }
+                intervals.add(new Interval(start, end));
+            }
+        }
+
+        return intervals;
+    }
+
+    private static long getSecondResult() {
+        List<Interval> intervals = (setIntervals(seeds));
+        long min = Long.MAX_VALUE;
+        int ind = 0;
+        for (Interval interval : intervals) {
+            for (long i = interval.start; i < interval.end; i++) {
+                min = Math.min(min, getSingleLocation(i));
+            }
+            // see progress
+            System.out.println("(" + ++ind + "/" + intervals.size() + ")");
+        }
+        return min;
+    }
+
+    private static long getFirstResult() {
         nextSeed = new Long[seeds.size()];
         int j = 0;
         for (Long seed : seeds) {
             nextSeed[j++] = seed;
         }
+        long min = Long.MAX_VALUE;
+        for (long seed : seeds) {
+            min = Math.min(min, getSingleLocation(seed));
+        }
+        return min;
+    }
+
+    static long getSingleLocation(long seed) {
         for (List<Map> maps : sourceToDestList) {
-            for (int i = 0; i < nextSeed.length; i++) {
-                long seed = nextSeed[i];
-                long nextValue = -1;
-                for (Map mapping : maps) {
-                    if (seed >= mapping.sourceRangeStart && seed < mapping.sourceRangeStart + mapping.range) {
-                        nextValue = seed + (mapping.destRangeStart - mapping.sourceRangeStart);
-                        break;
-                    } else {
-                        nextValue = seed;
-                    }
-                }
-                if (nextValue != -1) {
-                    nextSeed[i] = nextValue;
+            long nextValue = -1;
+            for (Map mapping : maps) {
+                if (seed >= mapping.sourceRangeStart && seed < mapping.sourceRangeStart + mapping.range) {
+                    nextValue = seed + (mapping.destRangeStart - mapping.sourceRangeStart);
+                    break;
+                } else {
+                    nextValue = seed;
                 }
             }
+            seed = nextValue;
         }
-        return Collections.min(Arrays.asList(nextSeed));
+        return seed;
     }
 
     public static void main(String[] args) {
         try {
             setData();
-            System.out.println(getResult());
+            long firstResult = getFirstResult();
+            long secondResult = getSecondResult();
+            System.out.println("First part: " + firstResult + "\nSecond part: " + secondResult);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
