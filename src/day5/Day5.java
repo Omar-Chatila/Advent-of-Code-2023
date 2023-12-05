8d3f98978703ef7096bf05673700b895c7b52043
omarc <sgtb45@gmail.com>
2023-12-05 13:42:19 +0100

    day 5 part 1

--- src/day5/Day5.java
+++ src/day5/Day5.java
@@ -4,7 +4,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Day5 {
@@ -23,13 +24,12 @@ public class Day5 {
     }
 
     static List<List<Map>> sourceToDestList = new ArrayList<>();
-    static List<List<Corresponding>> sourceDestList = new ArrayList<>();
+    static Long[] nextSeed;// = new ArrayList<>();
     static List<Long> seeds = new ArrayList<>();
 
     static void setData() throws IOException {
         for (int i = 0; i < 7; i++) {
             sourceToDestList.add(new ArrayList<>());
-            sourceDestList.add(new ArrayList<>());
         }
         List<String> lines = Files.readAllLines(Paths.get("src/day5/input.txt"));
         int index = -1;
@@ -48,28 +48,38 @@ public class Day5 {
                 }
             }
         }
+    }
+
+    private static long getResult() {
+        nextSeed = new Long[seeds.size()];
         int j = 0;
+        for (Long seed : seeds) {
+            nextSeed[j++] = seed;
+        }
         for (List<Map> maps : sourceToDestList) {
-            for (int i = 0; i < 100; i++) {
-                for (Map map : maps) {
-                    if (i >= map.sourceRangeStart && i < map.sourceRangeStart + map.range) {
-                        sourceDestList.get(j).add(new Corresponding(i, i + map.range));
+            for (int i = 0; i < nextSeed.length; i++) {
+                long seed = nextSeed[i];
+                long nextValue = -1;
+                for (Map mapping : maps) {
+                    if (seed >= mapping.sourceRangeStart && seed < mapping.sourceRangeStart + mapping.range) {
+                        nextValue = seed + (mapping.destRangeStart - mapping.sourceRangeStart);
+                        break;
+                    } else {
+                        nextValue = seed;
                     }
                 }
+                if (nextValue != -1) {
+                    nextSeed[i] = nextValue;
+                }
             }
-            sourceDestList.get(j).sort(Comparator.comparingLong(o -> o.source));
-            j++;
         }
-
-        for (List<Corresponding> correspondings : sourceDestList) {
-            System.out.println(correspondings);
-        }
-
+        return Collections.min(Arrays.asList(nextSeed));
     }
 
     public static void main(String[] args) {
         try {
             setData();
+            System.out.println(getResult());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
